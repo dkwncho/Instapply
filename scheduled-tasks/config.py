c7c5c7e0a8b4b72f273d783450de42118b517d65
Derek <170909852+dkwncho@users.noreply.github.com>
2024-08-13 11:02:30 -0400

    Created KEYWORDS dictionary to use for industry classification

---
+++ scheduled-tasks/config.py
@@ -0,0 +1,8 @@
+KEYWORDS = {
+    "Technology": ["Software", "Technology", "Technical", "Backend", "Data", "Embedded", "Frontend", "Machine Learning", "Cloud", "DevOps", "AI", "Artificial Intelligence", "Cybersecurity", "Quantitative"],
+    "Business": ["Finance", "Financial", "Business", "Investment", "Corporate", "Actuary", "Actuarial", "Banking", "Credit", "Revenue", "Market", "Markets", "Asset", "Equity", "Marketing", "Sales", "Consulting", "Risk", "Trading", "M&A", "Wealth", "Audit", "Tax"],
+    "Healthcare": ["Clinical", "Medical", "Medicine", "Health", "Healthcare", "Pharmacy", "Biological", "Biomedical", "Nursing", "Epidemiology", "Genetics"],
+    "Engineering": ["Engineering", "Engineer"],
+    "Government": ["Policy", "Government", "Affairs", "Advocacy", "Federal", "Civic", "Political", "Legislative", "Legal"],
+}
+
