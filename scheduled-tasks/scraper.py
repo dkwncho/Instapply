a204d4d9ef669e1c9d4d9551798f9994c9017322
Derek <170909852+dkwncho@users.noreply.github.com>
2024-08-13 14:53:46 -0400

    Optimized precision and accuracy of location scraping
    Before, location was scraped only from the metadata. Now, location is
    scraped directly from the website that contains the job posting with
    the former as a fallback.

--- scheduled-tasks/scraper.py
+++ scheduled-tasks/scraper.py
@@ -3,6 +3,7 @@ import re
 import requests
 from dotenv import load_dotenv
 from datetime import date, timedelta
+from bs4 import BeautifulSoup
 from config import KEYWORDS
 
 load_dotenv()
@@ -19,11 +20,17 @@ def extract_greenhouse_info(search_item):
         link = search_item.get("link")  
 
     try:
-        # Location is scraped from metadata
-        location = search_item["pagemap"]["metatags"][0]["og:description"]  
-    except KeyError:
-        location = "N/A"
-
+        # Location is scraped directly from website
+        r = requests.get(link)
+        soup = BeautifulSoup(r.content, 'html5lib')
+        location = soup.find('div', attrs = {'class':'location'}).text 
+    except AttributeError:
+        try:
+            # Location is scraped from metadata
+            location = search_item["pagemap"]["metatags"][0]["og:description"] 
+        except KeyError:
+            location = "N/A"
+            
     title_pattern = r'(.*) at ([^ ]+.*)'
     alt_title = search_item.get("title").removeprefix("Job Application for ")
     title_match = re.search(title_pattern, alt_title)
@@ -43,14 +50,12 @@ def extract_greenhouse_info(search_item):
         company = title_match.group(2).strip()
         if "..." in company:
             raise AttributeError
-        
     except AttributeError:
         try:
             # Company name is scraped from URL
             link_pattern = r'greenhouse\.io/([^/]+)/jobs'  
             link_match = re.search(link_pattern, link)
             company = link_match.group(1)
-            
         except AttributeError:
             company = "N/A"
     
@@ -110,8 +115,7 @@ def scrape_greenhouse():
                     }
                     job_postings.append(job_posting)
                 else:
-                    continue
-                
+                    continue     
         # Exits the loop when no more search results are found
         except TypeError:
             break
