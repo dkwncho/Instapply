0d63f43d742db5044f0cc7db7dc9c5fe05ad343a
Derek <170909852+dkwncho@users.noreply.github.com>
2024-08-13 21:49:13 -0400

    Improved table UI and added filtered industry tabs

---
+++ react-frontend/src/components/TabList.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const tabs = [
+    { key: 'Master', label: 'All' },
+    { key: 'Technology', label: 'Technology' },
+    { key: 'Business', label: 'Business' },
+    { key: 'Engineering', label: 'Engineering' },
+    { key: 'Healthcare', label: 'Healthcare' },
+    { key: 'Government', label: 'Government' }
+];
+
+const Tab = ({ tab, activeTab, onClick }) => (
+    <li key={tab.key} className={`border-b-2 ${activeTab === tab.key ? "border-indigo-600 text-indigo-600" : "border-white text-gray-500"}`}>
+        <button
+            role="tab"
+            className="py-2.5 px-4 rounded-lg duration-150 bg-white hover:text-indigo-600 hover:bg-gray-50 active:bg-gray-100 font-medium"
+            onClick={() => onClick(tab.key)}
+        >
+            {tab.label}
+        </button>
+    </li>
+);
+
+const TabList = ({ activeTab, setActiveTab }) => {
+    return (
+        <ul className="w-full mx-2 flex items-center gap-x-3 overflow-x-auto">
+            {tabs.map(tab => (
+                <Tab
+                    key={tab.key}
+                    tab={tab}
+                    activeTab={activeTab}
+                    onClick={setActiveTab}
+                />
+            ))}
+        </ul>
+    );
+};
+
+export default TabList;
--- react-frontend/src/components/Table.jsx
+++ react-frontend/src/components/Table.jsx
@@ -1,11 +1,22 @@
 import React, { useEffect, useState } from 'react';
-import { ConfigProvider, Table } from "antd";
-import { columns } from '../constants.jsx';
+import TabList from './TabList';
 
-function JobListings() {
+const filterByIndustry = (data, industry) => {
+    if (!Array.isArray(data)) {
+        return [];
+    }
+    return data.filter(item => item.industry.includes(industry));
+};
+
+function Table() {
     const [masterTable, setMasterTable] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
+    const [activeTab, setActiveTab] = useState("Master");
+    const [todayInternshipCount, setTodayInternshipCount] = useState(0);
+    const today = new Date();
+    var yesterday = new Date(today.setDate(today.getDate() - 1));
+    yesterday = `${String(yesterday.getMonth() + 1).padStart(2, '0')}/${String(yesterday.getDate()).padStart(2, '0')}/${yesterday.getFullYear()}`;
 
     useEffect(() => {
         fetch('https://instapply-api.vercel.app/api/master/cache')
@@ -16,7 +27,8 @@ function JobListings() {
                 return response.json();
             })
             .then(data => {
-                setMasterTable(data);
+                setMasterTable(data.reverse());
+                setTodayInternshipCount(data.filter(item => item.date === yesterday).length);
                 setLoading(false);
             })
             .catch(error => {
@@ -34,20 +46,47 @@ function JobListings() {
         return <p>Error fetching data: {error.message}</p>;
     }
 
+    const activeTabTable = activeTab === "Master" ? masterTable : filterByIndustry(masterTable, activeTab);
+
     return (
-        <ConfigProvider
-            theme={{
-                components: {
-                    Table: {
-                        headerBg: "white",
-                    },
-                },
-            }}
-        >
-            <Table dataSource={masterTable} columns = {columns} />
-        </ConfigProvider>
-        
-    );
+        <div className="max-w-screen-xl mx-auto px-4 md:px-8">
+            <div className="mx-2 max-w-lg text-left">
+                <h3 className="title text-xl font-bold sm:text-2xl">
+                    {todayInternshipCount} New Internships Added Today!
+                </h3>
+            </div>
+            <div className="text-sm mt-6 overflow-x-auto">
+                <TabList activeTab={activeTab} setActiveTab={setActiveTab} />
+                <div className="shadow-sm border rounded-lg">
+                    <table className="w-full table-auto rounded-lg text-left">
+                        <thead className="bg-gray-50 text-gray-600 font-medium border-b">
+                            <tr>
+                                <th className="w-4/12 py-4 pl-6">Job Title</th>
+                                <th className="w-3/12 py-4 pl-6">Company</th>
+                                <th className="w-3.5/12 py-4 pl-6">Location</th>
+                                <th className="w-1.5/12 py-4 pl-6">Date</th>
+                            </tr>
+                        </thead>
+                        <tbody className="text-gray-600 divide-y">
+                            {activeTabTable.map((item, idx) => (
+                                    <tr key={idx}>
+                                        <td className="pl-6 py-4 whitespace-wrap">
+                                            <a href={item.link}>
+                                                {item.title}
+                                            </a>
+                                        </td>
+                                        <td className="pl-6 py-4 whitespace-wrap">{item.company}</td>
+                                        <td className="pl-6 py-4 whitespace-wrap">{item.location}</td>
+                                        <td className="pl-6 py-4 whitespace-wrap">{item.date}</td>
+                                    </tr>
+                                ))
+                            }
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    )
 };
 
-export default JobListings;
+export default Table;
