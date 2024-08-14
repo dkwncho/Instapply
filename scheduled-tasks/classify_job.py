d11ad5413db3d3f834b0f41046f9713bc9b2499b
Derek <170909852+dkwncho@users.noreply.github.com>
2024-08-14 15:37:03 -0400

    Moved classify_job into its own module

---
+++ scheduled-tasks/classify_job.py
@@ -0,0 +1,13 @@
+import re
+from config import KEYWORDS
+
+def classify_job(title):
+    industry_list = []
+    for industry, keywords in KEYWORDS.items():
+        for keyword in keywords:
+            pattern = r'\b' + re.escape(keyword) + r'\b'
+            if re.search(pattern, title, re.IGNORECASE):
+                industry_list.append(industry)
+                break
+
+    return industry_list
